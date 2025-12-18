d7e919a4cfb7291663dd3b0a2b8d6bd5eb4d2283
Alo2118 <142913958+Alo2118@users.noreply.github.com>
2025-12-18 10:03:35 +0100

    Aggiornamento funzionalità

--- sw.js
+++ sw.js
@@ -1,33 +1,27 @@
-const CACHE_NAME = 'adhd-toolkit-v2';
+const CACHE_NAME = 'come-stai-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
   '/icon-192.png',
   '/icon-512.png',
-  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&family=JetBrains+Mono:wght@400;500&display=swap'
+  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&display=swap'
 ];
 
-// Install - cache files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aperta');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
       .then(() => self.skipWaiting())
   );
 });
 
-// Activate - clean old caches
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
-            console.log('Rimuovo vecchia cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -36,7 +30,6 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Fetch - serve from cache, fallback to network
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
@@ -50,9 +43,7 @@ self.addEventListener('fetch', event => {
           }
           const responseToCache = response.clone();
           caches.open(CACHE_NAME)
-            .then(cache => {
-              cache.put(event.request, responseToCache);
-            });
+            .then(cache => cache.put(event.request, responseToCache));
           return response;
         });
       })
